b8e1b15e9009e3b64d1b8ae41c6b2c8c5e9e9f89
Marzhan <m.zhylkaidarova@gmail.com>
2021-08-08 22:38:42 +0200

    cleaned up init file

--- __init__.py
+++ __init__.py
@@ -10,12 +10,7 @@ def create_app(config_file='config.py'):
     """ Create Flask app. """
     app = Flask(__name__)
     app.config.from_pyfile(config_file)
-    """
-    # App configs need to be refactored later to separate config file
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ll.db'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SECRET_KEY'] = 'yy&aqtrvt$otp1p30sjyam7^!&j0jrvljr(9ci99aek5tx@+s8'
-    """
+   
     db.init_app(app)
     migrate.init_app(app, db)
     login.init_app(app)
